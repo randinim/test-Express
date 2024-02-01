3ed80fababa661512e3df603736d457d7c9d37d5
Randini Maliksha <maliksharandini@gmail.com>
2024-02-01 15:31:26 +0530

    modified html into ejs

--- ejs/server.js
+++ ejs/server.js
@@ -4,22 +4,35 @@ const app = express();
 const PORT = 8082;
 //serve the  static file
 app.use(express.static(path.join(__dirname, "public")));
+//Set the view engine as ejs
+app.set("veiw engine", "ejs");
+
 //HOME ROUTE
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "views", "home.html"));
+  res.render("home.ejs");
 });
 
 //Render About ROUTE
 app.get("/about", (req, res) => {
-  res.sendFile(path.join(__dirname, "views", "about.html"));
+  res.render("about.ejs");
 });
 //Contact ROUTE
 app.get("/contact", (req, res) => {
-  res.sendFile(path.join(__dirname, "views", "contact.html"));
+  res.render("contact.ejs");
 });
 //Gallery ROUTE
 app.get("/gallery", (req, res) => {
-  res.sendFile(path.join(__dirname, "views", "gallery.html"));
+  res.render("gallery.ejs");
+});
+//userdata ROUTE
+app.get("/userData", (req, res) => {
+  const userData = {};
+  res.render("userData.ejs", {
+    username: "Randini",
+    age: 23,
+    isPremiumUser: true,
+    email: "randi@gmail.com",
+  });
 });
 //Start the server
 app.listen(PORT, console.log(`server is running on port ${PORT}`));
